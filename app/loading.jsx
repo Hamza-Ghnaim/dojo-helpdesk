faca677b1ffddab6d9c2de79c36f60426f7e42c9
dev <dev@varident.com>
2023-08-22 10:57:19 -0700

    initial commit

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+export default function loading() {
+    return(
+        <main className="text-center">
+            <h2 className="text-center">Loading...</h2>
+            <p>Hopefully for not too long :)</p>
+        </main>
+    )
+}
